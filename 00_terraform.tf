9d65156bd372ba34eee0bdeb6a087fdeafbe4576
Marko Locher <marko@locher.at>
2017-03-30 10:59:29 +0200

    Disable remote state until we get AWS credentials

--- 00_terraform.tf
+++ 00_terraform.tf
@@ -4,14 +4,14 @@
 terraform {
   required_version = "> 0.9.0"
 
-  backend "s3" {
+  /*backend "s3" {
     region = "us-east-1"
     bucket = "codeship-pro-terraform-state"
     key = "codeship-pro-terraform.tfstate"
     acl = "private"
     encrypt = "true"
-    ms_key_id = ""
+    kms_key_id = ""
     lock = "true"
     lock_table = "codeship-pro-terraform-lock"
-  }
+  }*/
 }
