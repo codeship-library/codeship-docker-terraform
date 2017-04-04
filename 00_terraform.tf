d35986ba8edc5aeed16c485239b5a2f9f4fdf477
Marko Locher <marko@locher.at>
2017-04-04 09:10:23 +0200

    Enable remote state on S3
    * Remove the (apparently deprecated) “lock” attribute
    * Fill in the KMS key ID

--- 00_terraform.tf
+++ 00_terraform.tf
@@ -4,14 +4,13 @@
 terraform {
   required_version = "> 0.9.0"
 
-  /*backend "s3" {
+  backend "s3" {
     region = "us-east-1"
     bucket = "codeship-pro-terraform-state"
     key = "codeship-pro-terraform.tfstate"
     acl = "private"
     encrypt = "true"
-    kms_key_id = ""
-    lock = "true"
+    kms_key_id = "arn:aws:kms:us-east-1:870119404647:key/365c1a98-3e6c-46ca-a502-0238d36671af"
     lock_table = "codeship-pro-terraform-lock"
-  }*/
+  }
 }
